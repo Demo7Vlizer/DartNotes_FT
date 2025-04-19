e881b07607890348057cb7dd3194d6f455a83a33
Demo7Vlizer <mintruik@gmail.com>
2025-04-19 22:24:43 +0530

    For , For in, For Each loops in Dart

--- 17_DartLession.dart
+++ 17_DartLession.dart
@@ -9,4 +9,54 @@
   }
 ----------------------------------------------------------------*/
 
+//---  for loop
+
+/*
+void main() {
+    int i;
+
+    for (i = 1; i <= 5; i++) {
+        print(i);
+    }
+}
+*/
+
+//----------------------------------------
+//-- For in loop
+
+/*
+  for (variable in expression) {
+    body of loop
+  }
+ */
+
+
+/*
+void main() {
+    List items = [10, 20, 30, 40, 50];
+
+    for (int item in items) {
+        print(item);
+    }
+}
+*/
+
+//----------------------------------------
+//-- forEach in loop
+
+/*
+collection.forEach (void fun(value){
+    body of loop
+});
+*/
+
+
+// forEach loop
+void main() {
+  List names = ['Sam', 'Mark', 'Dennis', 'Jack'];
+
+  names.forEach((name) {
+    print(name);
+  });
+}
 
