fa3fdd2b9a1ab29277945108d27757417b6aeeae
Demo7Vlizer <mintruik@gmail.com>
2025-04-20 12:58:20 +0530

    Named Parameters in Dart | Dart named parameters

--- 21_DartLession.dart
+++ 21_DartLession.dart
@@ -91,3 +91,104 @@ void test(int a, [int? b, int? c=0]) {
 
 }
 */
+
+//----------------------------------------------------------------------------
+//-- Named Parameters in Dart | Dart named parameters
+
+/*
+  void main(){
+    test(b:10, c:20, a:30);
+}
+
+void test({int? a, int? b, int? c}){   //-- Now we have named parameters after adding.. curly braces and make it nullable.. now we've to compulsarly to give the nemed either it'll show error.. 
+    print('a = $a');
+    print('b = $b');
+    print('c = $c');
+
+    /* -- print outputs.. 
+                      a = 30
+                      b = 10
+                      c = 20
+                    */
+}
+*/
+
+//----------------------------------------------------------------------------
+//-- All named parameters are optional.. 
+
+/*
+  void main(){
+    test(b:10, a:30);
+}
+
+void test({int? a, int? b, int? c}){   
+    print('a = $a');
+    print('b = $b');
+    print('c = $c');
+
+    /* -- print outputs.. 
+                      a = 30
+                      b = 10
+                      c = null
+                    */
+}
+*/
+
+//----------------------------------------------------------------------------
+
+/*
+ void main(){
+    test(b:10, a:30);
+}
+
+void test({int? a, int? b, int? c=0}){   
+    print('a = $a');
+    print('b = $b');
+    print('c = $c');
+
+    /* -- print outputs.. 
+                      a = 30
+                      b = 10
+                      c = 0
+                    */
+}
+*/
+
+//----------------------------------------------------------------------------
+
+/*
+void main(){
+    test(b:10, a:30, c:50);
+}
+
+void test({int? a, int? b, int? c=0}){   
+    print('a = $a');
+    print('b = $b');
+    print('c = $c');
+
+    /* -- print outputs.. 
+                      a = 30
+                      b = 10
+                      c = 50
+                    */
+}
+*/
+
+//----------------------------------------------------------------------------
+//-- If we want to make a some named  parameter required.. instead of optional.. 
+
+void main(){
+    test( b:10);      //-- Now we getting error.. if we didn't add the b named parameter.. 
+}
+
+void test({int? a=55, required int? b, int? c=0}){   
+    print('a = $a');
+    print('b = $b');
+    print('c = $c');
+
+    /* -- print outputs.. 
+                      a = 55
+                      b = 10
+                      c = 0
+                    */
+}
