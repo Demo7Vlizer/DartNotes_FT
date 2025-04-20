279225d55c9be3040d94d9f204af5a71b6074ffd
Demo7Vlizer <mintruik@gmail.com>
2025-04-20 13:31:42 +0530

    Arrow function

--- 22_DartLession.dart
+++ 22_DartLession.dart
@@ -9,6 +9,7 @@
  */
 
 //--------------------------------------------
+//-- Anonymous and Lambda function
 
 /*
 void main() {
@@ -36,6 +37,7 @@ void main() {
 
 //--------------------------------------------
 
+/*
 void main() {
   List names = ['Sam', 'Jack', 'Mark'];
 
@@ -43,3 +45,49 @@ void main() {
     print(name);               // Sam  Jack  Mark
   });
 }
+*/
+
+//----------------------------------------------------------------------------
+//-- Arrow Function in Dart
+
+/*
+//--- This is normal function.. 
+
+void main() {
+  int c = cube(5);
+
+  print('Cube of 5 = $c');         //  Cube of 5 = 125
+}
+
+int cube(int n) {
+  return n * n * n;
+}
+*/
+
+//******************************************************** */
+/*
+//--- This is Arrow function.. 
+
+void main() {
+  int c = cube(3);
+
+  print('Cube of 3 = $c');        //  Cube of 3 = 27
+}
+
+int cube(int n) => n * n * n;
+
+*/
+
+//******************************************************** */
+//--- Some of the key points that you must notice while using this notation.... 
+
+// /*
+void main() {
+  int c = cube(3);
+
+  print('Cube of 3 = $c');        //  Cube of 3 = 27
+}
+
+int cube(int n) => n * n * n;      // You must notice that you we cannot use return and curly braces.. 
+
+// */
