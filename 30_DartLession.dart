6d0fff19cd78991ca7c9569896a3524ef9cadcdf
Demo7Vlizer <mintruik@gmail.com>
2025-04-23 10:56:42 +0530

    Sections for initialization in constructor

---
+++ 30_DartLession.dart
@@ -0,0 +1,13 @@
+//--  Sections for initialization in constructor 
+
+/* 
+**"Field Initializers & Initializer List"**
+
+**"Constructor may has five sections"**  
+1) **Name of constructor** (Unnamed & named constructor)  
+2) **Parameter List or field initializer**  
+3) **Initializer list**  
+4) **Redirecting call using this()**  
+5) **Body** (Definition of constructor)
+
+ */
